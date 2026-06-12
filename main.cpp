bad787c5887da4fad8f21c09e5cc4746eae90779
Kojo Bailey <kojomolojo@gmail.com>
2026-06-12 06:57:04 +0100

    Add mode to play against computer

--- main.cpp
+++ main.cpp
@@ -14,7 +14,10 @@
 using sz = std::size_t;
 using u8 = std::uint8_t;
 
+#define loop while(true)
+
 enum class GameState : u8 {
+	Setup,
 	XTurn,
 	OTurn,
 	XWin,
@@ -117,6 +120,11 @@ public:
 		return {};
 	}
 
+	void place(const CellType cell_type, const sz index)
+	{
+		board[index] = cell_type;
+	}
+
 	[[nodiscard]] auto check_for_win() -> std::optional<GameState>
 	{
 		for (sz i = 0; i < row_count; i++) {
@@ -157,6 +165,17 @@ public:
 		return {};
 	}
 
+	[[nodiscard]] constexpr auto get_free_cells() const -> std::vector<sz>
+	{
+		std::vector<sz> result;
+		for (sz i = 0; i < board.size(); i++) {
+			if (board[i] == CellType::Empty) {
+				result.push_back(i);
+			}
+		}
+		return result;
+	}
+
 private:
 	static constexpr sz row_count = 3;
 	static constexpr sz column_count = 3;
@@ -182,48 +201,122 @@ private:
 	}
 };
 
+enum class PlayMode : u8 {
+	Undecided,
+	Player,
+	Computer,
+};
+
+GameState state = GameState::Setup;
+GameBoard board;
+PlayMode mode = PlayMode::Undecided;
+
+std::string user_input;
+
+auto handle_player_turn() -> bool
+{
+	auto cell_type = std::invoke([] -> GameBoard::CellType {
+		switch (state) {
+		case GameState::XTurn: return GameBoard::CellType::X;
+		case GameState::OTurn: return GameBoard::CellType::O;
+		default: std::unreachable();
+		}
+	});
+
+	loop {
+		std::print("> Player {}, enter a coordinate: ", GameBoard::cell_type_to_char(cell_type));
+		std::cin >> user_input;
+		std::println();
+
+		if (user_input[0] == 'q' || user_input[0] == 'e') {
+			return true;
+		}
+
+		auto place_success = board.place(cell_type, user_input);
+		if (!place_success.has_value()) {
+			std::println("[ERROR] {}", place_success.error());
+			std::println("Coordinates must follow the format [A-C][1-3] where the letters represent the rows and the numbers represent the columns, and cannot be placed in an occupied cell.");
+			std::println();
+			continue;
+		}
+		break;
+	}
+
+	return false;
+}
+
+void handle_computer_turn()
+{
+	const std::vector<sz> possible_choices = std::move(board.get_free_cells());
+
+	static std::mt19937 rng{std::random_device{}()};
+	std::uniform_int_distribution<sz> dist{0, possible_choices.size() - 1};
+
+	std::println("Computer is choosing...");
+	std::println();
+
+	const sz choice = possible_choices[dist(rng)];
+	board.place(GameBoard::CellType::O, choice);
+}
+
 auto main() -> int
 {
-	GameBoard board;
-	GameState state = GameState::XTurn;
-
-	std::string user_input;
-	
 	std::println("--- CLI Tic Tac Toe ---");
 	std::println("Get 3 of your symbol in a row, column, or diagonal to win!");
 	std::println("Input \"quit\" at any time to quit early.");
 	std::println();
 
-	while (true) {
-		board.draw();
+	while (mode == PlayMode::Undecided) {
+		std::print("> Play against another player or the computer? (p/c): ");
+		std::cin >> user_input;
 		std::println();
 
-		auto cell_type = std::invoke([state] -> GameBoard::CellType {
-			switch (state) {
-			case GameState::XTurn: return GameBoard::CellType::X;
-			case GameState::OTurn: return GameBoard::CellType::O;
-			default: std::unreachable();
-			}
-		});
+		switch (user_input[0]) {
+		case 'p':
+			mode = PlayMode::Player;
+			state = GameState::XTurn;
+			break;
+		case 'c':
+			mode = PlayMode::Computer;
+			break;
+		default:
+			std::println("Invalid input. Enter 'p' for player or 'c' for computer.");
+			std::println();
+		}
+	}
 
-		while (true) {
-			std::print("> Player {}, enter a coordinate: ", GameBoard::cell_type_to_char(cell_type));
+	if (mode == PlayMode::Computer) {
+		while (state == GameState::Setup) {
+			std::print("> You are Player X. Who plays first? (X/O): ");
 			std::cin >> user_input;
 			std::println();
 
-			if (user_input[0] == 'q' || user_input[0] == 'e') {
+			switch (user_input[0]) {
+			case 'X':
+				state = GameState::XTurn;
+				break;
+			case 'O':
+				state = GameState::OTurn;
+				break;
+			default:
+				std::println("Invalid input. Enter 'X' to play first or 'O' to go second.");
+				std::println();
+			}
+		}
+	}
+
+	loop {
+		board.draw();
+		std::println();
+
+		if (mode == PlayMode::Player || state == GameState::XTurn) {
+			bool quit_session = handle_player_turn();
+			if (quit_session) {
 				std::println("Quitting session... Thanks for playing!");
 				return 0;
 			}
-
-			auto place_success = board.place(cell_type, user_input);
-			if (!place_success.has_value()) {
-				std::println("[ERROR] {}", place_success.error());
-				std::println("Coordinates must follow the format [A-C][1-3] where the letters represent the rows and the numbers represent the columns, and cannot be placed in an occupied cell.");
-				std::println();
-				continue;
-			}
-			break;
+		} else {
+			handle_computer_turn();
 		}
 
 		auto maybe_win_result = board.check_for_win();
